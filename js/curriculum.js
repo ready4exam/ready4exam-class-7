2215d0a1519c40ad60b6f601d03d25ab0f954648
ready4exam <ready4urexam@gmail.com>
2025-12-23 15:31:21 +0530

    chore: update table_id for A Shirt in the Market → social_shirt_market_7_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -193,7 +193,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "A Shirt in the Market",
-        "table_id": "",
+        "table_id": "social_shirt_market_7_quiz",
         "section": "Social Science"
       },
       {
